2516364012bfebc382b58791395663c2d5d148d8
Aurélien Roux <aureroux13@me.com>
2017-01-30 20:51:16 +0000

    exercice 3

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello', function (req, res) {
+
+//EXERCICE 1 & 2
+// app.get('/hello', function (req, res) {
+//     res.send('<h1>Hello '  req.query.name + '!</h1>');
+// });
+
+
+//EXERCICE 3
+// app.get('/hello/:test', function (req, res){
+//     console.log(res);
+//     res.send('<h1>Hello ' + req.params.test +'</h1>')
+// })
+
+
+//EXERCICE 3
+app.get('/op/:tagId', function(req, res){
+    var temp = {
+        operator : req.params.tagId,
+        firstOperand: +req.query.num1,
+        secondOperand: +req.query.num2
+    };
     console.log(res);
-    res.send('<h1>Hello ' + req.query.name + '!</h1>');
+    
+    if (temp.operator === "add"){
+        temp.solution = temp.firstOperand + temp.secondOperand;
+    } else if (temp.operator === "sub"){
+        temp.solution = temp.firstOperand - temp.secondOperand;        
+    } else if (temp.operator === "div"){
+        temp.solution = temp.firstOperand / temp.secondOperand;
+    } else if (temp.operator === "mult"){
+        temp.solution = temp.firstOperand * temp.secondOperand;
+    } else {
+        temp.solution = res.status;
+    }
+    
+    res.send(JSON.stringify(temp));
 });
+
+
+
 
 
 //Boilerplate code to start up the web server
