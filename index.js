2ae8ad3065d3eaa25f9ac99a28136b83ed76fde1
Aurélien Roux <aureroux13@me.com>
2017-01-30 19:01:29 +0000

    exercice 2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get('/hello/good', function (req, res) {
+    res.send('Hello World test again again!');
 });
 
 
@@ -11,5 +11,5 @@ var server = app.listen(process.env.PORT, process.env.IP, function(){
     var host = server.address().address;
     var port = server.address().port;
     
-    console.log("Example app listening at http://", host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
